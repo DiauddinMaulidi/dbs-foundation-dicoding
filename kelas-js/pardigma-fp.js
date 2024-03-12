014c53460d7fcc4eab3c5e45a9fc40a52a8a16f7
Diak Uddin Maulidi <d14k.010@gmail.com>
2024-03-12 11:14:06 +0800

    contoh kalimat deklaratif dan imperatif

--- kelas-js/pardigma-fp.js
+++ kelas-js/pardigma-fp.js
@@ -1,3 +1,29 @@
+// contoh kalimat deklaratif dan imperatif
+// imperatif
+const insertionSortArray = (array) => {
+    for (let i = 1; i < array.length; i++) {
+        // console.log(i);
+        for (let j = i-1; j > -1; j--) {
+            // console.log(j);
+            if ( array[j+1] < array[j] ) {
+                [array[j+1], array[j]] = [array[j], array[j+1]]
+                // console.log(array);
+            }
+        }
+    }
+    return array
+}
+console.log(insertionSortArray([1,4,2,34,5]));
+
+// deklaratif
+const arraynya = [1,4,2,34,5];
+const compare = (a,b) => {
+    return a-b;
+}
+arraynya.sort(compare)
+console.log(arraynya);
+
+
 // Paradigma Functional Programing
     // adalah paradigma pemrograman yang menggunakan gaya deklaratif (ringkas dan jelas) yang dengan pendekatam What To Solve, tidak menggunakan gaya imperatif yang penulisannya lebih panjang dengan pendekatan How To Solve
     
